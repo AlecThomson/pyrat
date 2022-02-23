2dcf8bc84bb113d6d856b31025ed98b150727711
Thomson, Alec (S&A, Kensington) <alec.thomson@csiro.au>
2022-02-23 08:50:18 +0800

    Move to dir

--- __init__.py
+++
@@ -1,8 +0,0 @@
-VERSION = '0.1.0.0'
-
-import Messenger  # lint:ok
-import RAData  # lint:ok
-import RAImage  # lint:ok
-import Constants  # lint:ok
-import ParsetManager  # lint:ok
-import BaseRAData  # lint:ok
---
+++ pyrat/__init__.py
@@ -0,0 +1,8 @@
+VERSION = '0.1.0.0'
+
+from pyrat import Messenger as Messenger  # lint:ok
+from pyrat import RAData as RAData  # lint:ok
+from pyrat import RAImage as RAImage  # lint:ok
+from pyrat import Constants as Constants  # lint:ok
+from pyrat import ParsetManager as ParsetManager  # lint:ok
+from pyrat import BaseRAData as BaseRAData  # lint:ok
